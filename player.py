2c53552d0955aa1dc08f2c4e0d7bf675e8ac3b3f
Max Pattillo <maxkpat@gmail.com>
2025-12-18 15:57:56 -0800

    changed movement keys

--- player.py
+++ player.py
@@ -30,13 +30,13 @@ class Player(CircleShape):
         
         self.shot_cooldown_timer -= dt
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             if self.shot_cooldown_timer > 0:
